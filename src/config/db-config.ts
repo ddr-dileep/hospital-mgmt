a1427dfd419d9212ba5baf604f055231bebc0bf5
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-14 14:54:26 +0530

    fix : database configs

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -4,11 +4,11 @@ const dbConnection = async () => {
   try {
     const dbConnectionString = process.env.DB_CONNECTION_URL;
 
-    const check = await mongoose.connect(dbConnectionString!, {
+    await mongoose.connect(dbConnectionString!, {
       dbName: "hospital_management",
     });
 
-    console.log("connected to DB successfully", check.Collection.dbName);
+    console.log("connected to DB successfully");
   } catch (error) {
     console.log("error in db connection", error);
   }
