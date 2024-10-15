47175e850f04b869ab5b35ab7760c692bee48c13
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-15 18:52:50 +0530

    feat : admin controller for create admin

--- src/controllers/auth/admin-controllers.ts
+++ src/controllers/auth/admin-controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Admin from "../../models/auth/admin-model";
 import Owner from "../../models/auth/owner-model";
 import { API_RESPONSE } from "../../utils/api-response";
+import Hospital from "../../models/hospital/hospital-model";
 
 export const adminControllers = {
   getAdmins: async (req: Request | any, res: Response): Promise<any> => {
@@ -22,6 +23,40 @@ export const adminControllers = {
         admins,
         message: "Admins fetched successfully",
       });
+    } catch (error) {
+      res.status(500).json(API_RESPONSE.ERROR(error));
+    }
+  },
+
+  createAdmin: async (req: Request | any, res: Response): Promise<any> => {
+    try {
+      const hospital = await Hospital.findById(req?.params?.hospitalId);
+
+      console.log("hospial", hospital);
+
+      if (!hospital) {
+        return res
+          .status(404)
+          .json(API_RESPONSE.ERROR({ message: "Hospital is not exits" }));
+      }
+
+      if (hospital?.isDeleted) {
+        return res
+          .status(403)
+          .json(
+            API_RESPONSE.ERROR({ message: "Hospital is deleted/not fount" })
+          );
+      }
+
+      const admin = new Admin({
+        ...req.body,
+        createdByUser: "Owner",
+        createdBy: req.user.id,
+        hospital: hospital?._id,
+      });
+
+      await admin.save();
+      res.send(API_RESPONSE.SUCCESS({ admin }));
     } catch (error) {
       res.status(500).json(API_RESPONSE.ERROR(error));
     }
