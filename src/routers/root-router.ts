4b4c986a9ee805c27795c7cf0ddb2c70ba6cbfa6
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-15 17:07:57 +0530

    feat : added admin router

--- src/routers/root-router.ts
+++ src/routers/root-router.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import userRouter from "./auth/user-router";
 import ownerRouter from "./auth/owner-auth";
 import hospitalRouter from "./hospital/hospital-router";
+import adminRouter from "./auth/admin-router";
 
 const rootRouter = Router();
 
 rootRouter.use("/auth", userRouter);
 
-// owner routes
+// auth routes
 rootRouter.use("/owner", ownerRouter);
+rootRouter.use("/admin", adminRouter);
 
 // hospital routes
-rootRouter.use("/hospital",  hospitalRouter);
+rootRouter.use("/hospital", hospitalRouter);
 
 export default rootRouter;
