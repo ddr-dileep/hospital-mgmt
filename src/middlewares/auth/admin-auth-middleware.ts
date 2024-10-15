9f579365b0034c0275736ef19061edb45ace7348
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-15 18:53:06 +0530

    feat :  admin middleware for adding admin

---
+++ src/middlewares/auth/admin-auth-middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { API_RESPONSE } from "../../utils/api-response";
+
+export const adminMiddleware = {
+  create: (req: Request, res: Response, next: NextFunction): any => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json(
+        API_RESPONSE.ERROR({
+          name: name ? undefined : "Name is required",
+          email: email ? undefined : "Email is required",
+          password: password ? undefined : "Password is required",
+          message: "All fields are required",
+        })
+      );
+    }
+    next();
+  },
+};
