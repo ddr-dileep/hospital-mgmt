ae228da301f2aa704f879871585dcb5d363a0cf0
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-15 11:52:47 +0530

    feat : get owner info from token

--- src/controllers/auth/owner-controllers.ts
+++ src/controllers/auth/owner-controllers.ts
@@ -49,4 +49,30 @@ export const ownerController = {
       res.status(500).json(API_RESPONSE.ERROR(error));
     }
   },
+
+  ownerInfo: async (req: Request | any, res: Response): Promise<any> => {
+    try {
+      const owner = await Owner.findById(req.user.id).select(
+        "name email createdAt isDeleted updatedAt"
+      );
+
+      if (!owner) {
+        return res
+          .status(404)
+          .json(API_RESPONSE.ERROR({ message: "Owner not found" }));
+      }
+
+      if (req.user.isDeleted) {
+        return res.status(403).json(
+          API_RESPONSE.ERROR({
+            message: "Owner account is deleted",
+          })
+        );
+      }
+
+      res.status(200).json(API_RESPONSE.SUCCESS({ owner }));
+    } catch (error: any) {
+      res.status(500).json(API_RESPONSE.ERROR(error));
+    }
+  },
 };
--- src/routers/auth/owner-auth.ts
+++ src/routers/auth/owner-auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ownerController } from "../../controllers/auth/owner-controllers";
 import { ownerMiddleware } from "../../middlewares/auth/owner-auth-middleware";
+import { authTokenMiddleware } from "../../middlewares/authTokenMiddleware";
 
 const ownerRouter = Router();
 export default ownerRouter;
@@ -12,3 +13,9 @@ ownerRouter.post(
 );
 
 ownerRouter.post("/login", ownerMiddleware.login, ownerController.ownnerLogin);
+
+ownerRouter.get(
+  "/get-owners-info",
+  authTokenMiddleware,
+  ownerController.ownerInfo
+);
