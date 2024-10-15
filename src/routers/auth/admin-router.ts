28e60f439a492a7ce1bb8c679c131c3c8aee362c
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-15 18:53:41 +0530

    feat : admin create api

--- src/routers/auth/admin-router.ts
+++ src/routers/auth/admin-router.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { adminControllers } from "../../controllers/auth/admin-controllers";
 import { authTokenMiddleware } from "../../middlewares/auth/authTokenMiddleware";
+import { adminMiddleware } from "../../middlewares/auth/admin-auth-middleware";
 
 const adminRouter = Router();
 export default adminRouter;
 
 adminRouter.get("/get-admins", authTokenMiddleware, adminControllers.getAdmins);
+
+adminRouter.post(
+  "/create/:hospitalId",
+  adminMiddleware.create,
+  authTokenMiddleware,
+  adminControllers.createAdmin
+);
