f9cc20dba30428f3e4466b676bf519b5cd41e8a5
Dileep Raloti <ddr.dileepraloti@gmail.com>
2024-10-22 12:33:43 +0530

    fix : create doctor api

--- src/controllers/auth/doctor-controllers.ts
+++ src/controllers/auth/doctor-controllers.ts
@@ -1,16 +1,36 @@
 import { Request, Response } from "express";
 import { API_RESPONSE } from "../../utils/api-response";
+import Doctor from "../../models/auth/doctor-model";
+import { hashPassword } from "../../utils/bcrypt";
 
 export const doctorController = {
-  createDoctor: (req: Request, res: Response) => {
+  createDoctor: async (req: Request, res: Response) => {
     try {
-      // Implement doctor creation logic here
-      res.status(200).json(API_RESPONSE.SUCCESS({ check: true }));
-    } catch (error) {}
+      const newDoctor = new Doctor({
+        ...req.body,
+        password: await hashPassword(req.body?.password),
+      });
+
+      await newDoctor.save();
+
+      const doctor = newDoctor;
+      doctor.password = undefined; // Remove password from response before sending back to client
+
+      res.status(200).json(
+        API_RESPONSE.SUCCESS({
+          doctor,
+          message: "Doctor added successfully",
+        })
+      );
+    } catch (error) {
+      res.status(400).json(API_RESPONSE.ERROR(error));
+    }
   },
+
   getDoctors: () => {
     // Implement doctor retrieval logic here
   },
+
   getDoctorById: () => {
     // Implement doctor retrieval by ID logic here
   },
--- src/middlewares/auth/doctor-auth-middlewares.ts
+++ src/middlewares/auth/doctor-auth-middlewares.ts
@@ -1,8 +1,33 @@
 import { NextFunction, Request, Response } from "express";
+import { API_RESPONSE } from "../../utils/api-response";
 
 export const doctorMiddleware = {
-  create: (req: Request, res: Response, next: NextFunction) => {
-    // Implement logic to validate doctor data
+  create: (req: Request, res: Response, next: NextFunction): any => {
+    const { name, email, password, contactNumber, experience, specialization } =
+      req.body;
+
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !contactNumber ||
+      !experience ||
+      !specialization
+    ) {
+      return res.status(403).json(
+        API_RESPONSE.ERROR({
+          message: "All fields are required",
+          name: name ? undefined : "Name is required",
+          email: email ? undefined : "Email is required",
+          password: password ? undefined : "Password is required",
+          contactNumber: contactNumber ? undefined : "Contact is required",
+          experience: experience ? undefined : "Experience is required",
+          specialization: specialization
+            ? undefined
+            : "Specialization is required",
+        })
+      );
+    }
     next();
   },
 };
